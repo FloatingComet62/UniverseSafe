79e7c871c74dc89bad4b90248d2fe10053c75535
FloatingComet62 <67373626+FloatingComet62@users.noreply.github.com>
2021-08-29 22:13:30 +0530

    Fixed the negative ping
    Now the negative ping will not show up

--- commands/ping.js
+++ commands/ping.js
@@ -7,6 +7,7 @@ module.exports =
     execute( message , args )
     {
         var ping = message.createdTimestamp - Date.now() + " ms`";
+        var ping = ping.replace(/-/g, "");
         var Embed = new Discord.MessageEmbed()
         .setDescription( "Pong! `" + ping );
         message.channel.send( Embed );
