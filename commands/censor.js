47e3179563f4b8b5931803522a617abf1b82b372
FloatingComet62 <67373626+FloatingComet62@users.noreply.github.com>
2021-11-02 09:44:51 +0530

    Fixed some common bugs faced
    I totally forgot to define the Profanity Variable. My Bad

--- commands/censor.js
+++ commands/censor.js
@@ -36,6 +36,7 @@ module.exports =
               var Info = JSON.stringify(response.data, null, 2);
               Info = JSON.parse(Info);
               var ToxicPercent = Math.round(Info.attributeScores.TOXICITY.spanScores[0].score.value*100);
+	      var ProfanityPercent = Math.round(Info.attributeScores.PROFANITY.spanScores[0].score.value*100);
                   Mute(ToxicPercent, 90, 100, '10m', Info , 'Toxic');
                   Mute(ToxicPercent, 85, 90, '5m', Info, 'Toxic');
                   Mute(ProfanityPercent, 90, 100, '10m', Info, 'Explict');
@@ -47,12 +48,13 @@ module.exports =
           throw err;
         });
       async function Mute(PercentVariable, PercentMin, PercentMax, time, Info, type){
-        if(ToxicPercentVariable >= ToxicPercentMin && ToxicPercentVariable < ToxicPercentMax){
+        if(PercentVariable >= PercentMin && PercentVariable < PercentMax){
           if(message.deletable){
               message.delete().catch(err => {});
               const ServerRef = DB.ref(message.guild.id)
               var MutedID = '';
               var ReportChID = '';
+	      var Loop = 1
               await ServerRef.once('value',   function(snapshot) {
                 snapshot.forEach(function(childSnapshot) {
                     Val = childSnapshot.val();
@@ -78,7 +80,7 @@ module.exports =
                   message.guild.members.cache.get(message.author.id).roles.remove(mutedRole);
                 } , ms(time)
               );
-              var Embed = new Discord.MessageEmbed()
+		var Embed = new Discord.MessageEmbed()
               .setTitle('Muted')
               .setDescription('Muted <@!' + message.author.id + '> for being '+ type.toLowerCase() +'\n' + type + ': `' + PercentVariable + '%`\nMute Duration: `' + time + '`');
               message.channel.send({
@@ -86,14 +88,14 @@ module.exports =
                     Embed
                 ]
               });
-			var EmbedTarget = new Discord.MessageEmbed()
+		var EmbedTarget = new Discord.MessageEmbed()
               .setTitle('Muted')
               .setDescription('You have been muted on **' + message.guild.name + '**\nToxicity: `' + Math.round(Info.attributeScores.TOXICITY.spanScores[0].score.value * 100) + '%`\nProfanity: `' + Math.round(Info.attributeScores.PROFANITY.spanScores[0].score.value * 100) + '%`\nThreatening: `' + Math.round(Info.attributeScores.THREAT.spanScores[0].score.value * 100) + '%`\nInsulting: `' + Math.round(Info.attributeScores.INSULT.spanScores[0].score.value * 100) + '%`');
               message.author.send({
                 embeds : [
                   EmbedTarget
                 ]
-              })
+              });
             }
           }
       }
